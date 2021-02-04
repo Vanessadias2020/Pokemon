7e292f39d630de1a9e2d214c1a9aa3c21f2d128c
Vanessadias2020 <cat.vanessa@hotmail.com>
2021-02-03 22:31:38 -0300

    first commit

---
+++ js/funcoes-cinemas.js
@@ -0,0 +1,13 @@
+function Esconder(){
+ // $("#resumo").toggle();
+ //  $("#resume").show();
+ // $("#resumo").hide();
+
+ //$("#resumo").slidetoggle();
+ // $("#resumo").slidetDown();
+ // $("#resumo").slideUp();
+
+ //$("#resumo").fadeToggle(1000);
+ //$("#resumo").fadeIn();
+  $("#resumo").fadeOut();
+}
