c68c445c0a3a65d388a8b35d571d4a71b107cc05
Vanessadias2020 <cat.vanessa@hotmail.com>
2021-02-05 22:49:00 -0300

    Aplicando JQuery

--- js/funcoes-cinemas.js
+++ js/funcoes-cinemas.js
@@ -9,7 +9,22 @@ function Esconder(){
 
  //$("#resumo").fadeToggle(1000);
  //$("#resumo").fadeIn();
-  $("#resumo").fadeOut();
+ // $("#resumo").fadeOut();
 }
 
 	$(".swipebox").swipebox();
+ 
+
+
+  function RetornarEndereco(){
+    let cep=$("#cep").val();
+    let url="https://viacep.com.br/ws/"+ cep +"/json";
+    
+    $.getJSON(url,function(dados){
+      $("#endereco-completo").append(dados.logradouro + "<br>" + dados.localidade + " - " + dados.uf );
+    });
+  }
+  document.querySelector("#BuscarCep").addEventListener("click",function(){
+
+    RetornarEndereco();
+  });
